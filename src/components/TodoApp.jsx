f5f9dd1505d3a3e98c1984aa251b7b5a6629caa2
FranklinZ12 <franklinq8@gmail.com>
2022-03-21 11:42:51 -0500

    optimizando todoApp

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -4,6 +4,7 @@ import { useForm } from '../hooks/useForm';
 import "../styles/todo.css";
 import { ToastContainer, toast, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Todolist from './Todolist';
 
 const init = () => {
     return JSON.parse(localStorage.getItem('todos')) || [];
@@ -79,22 +80,11 @@ const TodoApp = () => {
             <hr />
             <div className="row">
                 <div className="col-7">
-                    <ul className='list-group list-group-flush'>
-                        {todos.map((todo, i) => (
-                            <li
-                                key={todo.id}
-                                className="list-group-item"
-                            >
-                                <p className={`${todo.done && 'complete'}`}
-                                    onClick={() => handleToggle(todo.id)}
-                                >{i + 1}. {todo.desc}</p>
-                                <button
-                                    className="btn btn-danger"
-                                    onClick={(() => handleDelete(todo.id))}
-                                >Borrar</button>
-                            </li>
-                        ))}
-                    </ul>
+                    <Todolist
+                        todos={todos}
+                        handleDelete={handleDelete}
+                        handleToggle={handleToggle}
+                    />
                 </div>
                 <div className="col-5">
                     <h4>Agregar TODO</h4>
